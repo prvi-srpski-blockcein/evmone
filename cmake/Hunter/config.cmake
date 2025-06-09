da43aeb0caeee862ae9a581bbac790ec1bb040fe
Luka Radisavljevic <lukaradisavljevic30@gmail.com>
2025-06-10 01:32:01 +0200

    [init] Initial commit with configuration for qevm EVMONE
    Replaced evmc submodule with our fork of it. Added intx for better developer experience and more insight; should remove it later

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -11,6 +11,9 @@ hunter_cmake_args(
 
 hunter_config(
     intx
+    # VERSION 0.14.0
+    # URL https://github.com/chfast/intx/archive/v0.14.0.tar.gz
+    # SHA1 8f5c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c0c
     VERSION 0.13.0
     URL https://github.com/chfast/intx/archive/v0.13.0.tar.gz
     SHA1 3e868e3018fe9f2b2f067442c879e4f312d2d707
